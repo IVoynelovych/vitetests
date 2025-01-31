a63aeba8bcc4c696479472dccd946157e907ba78
IVoynelovych <121500133+IVoynelovych@users.noreply.github.com>
2025-01-31 20:53:09 +0000

    Deploying to gh-pages from @ IVoynelovych/vitetests@631d3e4379d36b6294c1f18d87e5cd33b43b05b3 🚀

--- assets/main-B9-p8Gvn.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./changetorigistrated-0usOyNcO.js";window.addEventListener("load",()=>{a()});
---
+++ assets/main-UIHQNVnb.js
@@ -0,0 +1,1 @@
+import{c as a}from"./changetorigistrated-B8TaOUy0.js";window.addEventListener("load",()=>{a()});
